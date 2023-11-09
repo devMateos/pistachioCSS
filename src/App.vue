413f2726fb44b2d52d9a7eeeae9dc6a268b23a20
Antonio Mateos <antoniomateosmariscal@gmail.com>
2023-11-09 14:27:11 +0100

    First commit: Initializing PistachioCSS

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <router-link to="/">Home</router-link><br>
+    <router-link to="/init">Init</router-link>
+  </div>
+  <router-view />
+</template>
+
+<style scoped></style>
