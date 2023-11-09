8571012c75710d9b2c533ea8a3f133d00c79b85e
Antonio Mateos <antoniomateosmariscal@gmail.com>
2023-11-09 21:46:03 +0100

    Adding assets: logos & fonts. Creating ScreenSplash component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
+import SplashScreen from './components/SplashScreen.vue';
+
 
 </script>
 
 <template>
+  <SplashScreen />
   <div>
     <router-link to="/">Home</router-link><br>
     <router-link to="/init">Init</router-link>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="splashscreen">
+    <img src="@/assets/img/isotype-logo-pistachiocss.webp" alt="PistachioCSS Isotype">
+    <img src="@/assets/img/text-logo-pistachiocss.webp" alt="PistachioCSS logotype">
+    <h1>PistachioCSS: The best way to create your design system for CSS</h1>
+  </div>
+</template>
+
+<style scoped>
+.splashscreen {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  position: absolute;
+  width: 100vw;
+}
+
+img {
+  width: clamp(100px, 60%, 300px);
+}
+</style>
