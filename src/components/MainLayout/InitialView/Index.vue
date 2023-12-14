c1079c562135a5cd6442e8d5c08bac39c77e1a72
Antonio Mateos <antoniomateosmariscal@gmail.com>
2023-12-14 15:44:47 +0100

    fix: Add buttons a padding bottom to solve problem in mobile version

--- src/components/MainLayout/InitialView/Index.vue
+++ src/components/MainLayout/InitialView/Index.vue
@@ -95,7 +95,7 @@ function copy(text) {
         />
       </section>
       
-      <div class="form__button-container pb-L">
+      <div class="form__button-container pb-XL">
         <CopyButton class="button copy-complete-code"
           :text="'Copy all'"
           v-if="showCode"
